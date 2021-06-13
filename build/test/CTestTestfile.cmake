02ffe6a451cdceb052f051e1afd876d112031db2
Syunya Suzuki <syunya_0617@outlook.jp>
2021-06-13 17:49:28 +0900

    git rm -r --cached .

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ssuzuki/workspace/PGM/test
-# Build directory: /home/ssuzuki/workspace/PGM/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_all "/home/ssuzuki/workspace/PGM/build/test/tests")
-set_tests_properties(test_all PROPERTIES  _BACKTRACE_TRIPLES "/home/ssuzuki/workspace/PGM/test/CMakeLists.txt;3;add_test;/home/ssuzuki/workspace/PGM/test/CMakeLists.txt;0;")
